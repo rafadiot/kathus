4db2f52e2167b52b5dae2e8799a6aacc8f0ce1fe
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-05 17:18:06 +0700

    Add the stubs

--- resources/stubs/module/Providers/KathusServiceProvider.php
+++ resources/stubs/module/Providers/KathusServiceProvider.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace DummyNamespace\Providers;
+
+use Kathus\Support\ServiceProvider;
+
+class DummyProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(kathus_path('DummySlug', 'ResourcesLangMapping', 'DummyLocation'), 'DummySlug');
+        $this->loadViewsFrom(kathus_path('DummySlug', 'ResourcesViewsMapping', 'DummyLocation'), 'DummySlug');
+        $this->loadMigrationsFrom(kathus_path('DummySlug', 'DatabaseMigrationsMapping', 'DummyLocation'), 'DummySlug');
+        $this->loadConfigsFrom(kathus_path('DummySlug', 'ConfigMapping', 'DummyLocation'));
+        $this->loadFactoriesFrom(kathus_path('DummySlug', 'DatabaseFactoriesMapping', 'DummyLocation'));
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
--- resources/stubs/module/Providers/RouteServiceProvider.php
+++ resources/stubs/module/Providers/RouteServiceProvider.php
@@ -1,1 +1,75 @@
 <?php
+
+namespace DummyNamespace\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to your controller routes.
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'DummyNamespace\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the module.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapWebRoutes();
+        $this->mapApiRoutes();
+        //
+
+    }
+
+    /**
+     * Define the "web" routes for the module.
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::group([
+            'middleware' => 'web',
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require kathus_path('DummySlug', 'RoutesMapping/web.php', 'DummyLocation');
+        });
+    }
+
+    /**
+     * Define the "api" routes for the module.
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::group([
+            'middleware' => 'api',
+            'namespace' => $this->namespace,
+            'prefix' => 'api',
+        ], function ($router) {
+            require kathus_path('DummySlug', 'RoutesMapping/api.php', 'DummyLocation');
+        });
+    }
+}
