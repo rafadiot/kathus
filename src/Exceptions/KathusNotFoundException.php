40871d89a8344311f7f8d3f34994b26409f21855
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-12 17:36:47 +0700

    Define exception

---
+++ src/Exceptions/KathusNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kathus\Exceptions;
+
+class KathusNotFoundException extends \Exception
+{
+    /**
+     * KathusNotFoundException constructor.
+     * @param $slug
+     */
+    public function __construct($slug)
+    {
+        parent::__construct('Module with slug name [' . $slug . '] not found');
+    }
+}
