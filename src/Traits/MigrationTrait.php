8f556b615d8e1a8def025e6416384580c47ee71c
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-12 17:37:30 +0700

    Define migration trait

--- src/Traits/MigrationTrait.php
+++ src/Traits/MigrationTrait.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace Kathus\Traits;
+
+trait MigrationTrait
+{
+    /**
+     * Require (once) all migration files for the supplied module.
+     *
+     * @param $module
+     * @throws \Kathus\Exceptions\KathusNotFoundException
+     */
+    protected function requireMigrations($module)
+    {
+        $path = $this->getMigrationPath($module);
+
+        $migrations = $this->laravel['files']->glob($path.'*_*.php');
+
+        foreach ($migrations as $migration) {
+            $this->laravel['files']->requireOnce($migration);
+        }
+    }
+
+    /**
+     * Get migration directory path.
+     *
+     * @param $module
+     * @return string
+     * @throws \Kathus\Exceptions\KathusNotFoundException
+     */
+    protected function getMigrationPath($module)
+    {
+        return kathus_path($module, 'Database/Migrations');
+    }
+}
