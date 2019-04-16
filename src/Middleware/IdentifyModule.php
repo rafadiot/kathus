c85a5c055ef5a65a68d3aad1540d731a7dec1b23
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-16 16:15:03 +0700

    Modify middleware

--- src/Middleware/IdentifyModule.php
+++ src/Middleware/IdentifyModule.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace Kathus\Middleware;
+
+use Kathus\RepositoryManager;
+use Closure;
+
+class IdentifyModule
+{
+    /**
+     * @var RepositoryManager
+     */
+    protected $module;
+
+    /**
+     * IdentifyModule constructor.
+     *
+     * @param RepositoryManager $module
+     */
+    public function __construct(RepositoryManager $module)
+    {
+        $this->module = $module;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $slug = null)
+    {
+        $request->session()->flash('kathus', $this->module->where('slug', $slug));
+
+        return $next($request);
+    }
+}
