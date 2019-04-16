27e4c5927a041c4d37fd728deba0df67094faa14
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-16 16:14:41 +0700

    Modify facades

--- src/Facades/Kathus.php
+++ src/Facades/Kathus.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace Kathus\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Kathus extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'kathus';
+    }
+}
