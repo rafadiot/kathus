e878659a464897ac62bc7d20164df39f13d9ea75
minhtuanchannhan <minhtuanchannhan@gmail.com>
2019-04-16 16:15:37 +0700

    Modify repository

--- src/Repositories/LocalRepository.php
+++ src/Repositories/LocalRepository.php
@@ -1,1 +1,309 @@
 <?php
+
+namespace Kathus\Repositories;
+
+use Illuminate\Support\Str;
+
+class LocalRepository extends Repository
+{
+    /**
+     * Get all modules.
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function all()
+    {
+        return $this->getCache()->sortBy('order');
+    }
+
+    /**
+     * Get all module slugs.
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function slugs()
+    {
+        $slugs = collect();
+
+        $this->all()->each(function ($item, $key) use ($slugs) {
+            $slugs->push(strtolower($item['slug']));
+        });
+
+        return $slugs;
+    }
+
+    /**
+     * Get modules based on where clause.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function where($key, $value)
+    {
+        return collect($this->all()->where($key, $value)->first());
+    }
+
+    /**
+     * Sort modules by given key in ascending order.
+     *
+     * @param string $key
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function sortBy($key)
+    {
+        $collection = $this->all();
+
+        return $collection->sortBy($key);
+    }
+
+    /**
+     * Sort modules by given key in ascending order.
+     *
+     * @param string $key
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function sortByDesc($key)
+    {
+        $collection = $this->all();
+
+        return $collection->sortByDesc($key);
+    }
+
+    /**
+     * Determines if the given module exists.
+     *
+     * @param string $slug
+     * @return bool
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function exists($slug)
+    {
+        return ($this->slugs()->contains($slug) || $this->slugs()->contains(Str::slug($slug)));
+    }
+
+    /**
+     * Returns count of all modules.
+     *
+     * @return int
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function count()
+    {
+        return $this->all()->count();
+    }
+
+    /**
+     * Get a module property value.
+     *
+     * @param string $property
+     * @param null $default
+     * @return mixed|null
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function get($property, $default = null)
+    {
+        list($slug, $key) = explode('::', $property);
+
+        $module = $this->where('slug', $slug);
+
+        return $module->get($key, $default);
+    }
+
+    /**
+     * Set the given module property value.
+     *
+     * @param string $property
+     * @param mixed $value
+     * @return bool|int
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function set($property, $value)
+    {
+        list($slug, $key) = explode('::', $property);
+
+        $cachePath = $this->getCachePath();
+        $cache = $this->getCache();
+        $module = $this->where('slug', $slug);
+
+        if (isset($module[$key])) {
+            unset($module[$key]);
+        }
+
+        $module[$key] = $value;
+        $module = collect([$module['basename'] => $module]);
+        $merged = $cache->merge($module);
+        $content = json_encode($merged->all(), JSON_PRETTY_PRINT);
+
+        return $this->files->put($cachePath, $content);
+    }
+
+    /**
+     * Get all enabled modules.
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function enabled()
+    {
+        return $this->all()->where('enabled', true);
+    }
+
+    /**
+     * Get all disabled modules.
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function disabled()
+    {
+        return $this->all()->where('enabled', false);
+    }
+
+    /**
+     * Check if specified module is enabled.
+     *
+     * @param string $slug
+     * @return bool
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function isEnabled($slug)
+    {
+        $module = $this->where('slug', $slug);
+
+        return $module['enabled'] === true;
+    }
+
+    /**
+     * Check if specified module is disabled.
+     *
+     * @param string $slug
+     * @return bool
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function isDisabled($slug)
+    {
+        $module = $this->where('slug', $slug);
+
+        return $module['enabled'] === false;
+    }
+
+    /**
+     * Enables the specified module.
+     *
+     * @param string $slug
+     * @return bool|int
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function enable($slug)
+    {
+        return $this->set($slug . '::enabled', true);
+    }
+
+    /**
+     * Disables the specified module.
+     *
+     * @param string $slug
+     * @return bool|int
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function disable($slug)
+    {
+        return $this->set($slug . '::enabled', false);
+    }
+
+    /**
+     * Update cached repository of module information.
+     *
+     * @return bool|int
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function optimize()
+    {
+        $cachePath = $this->getCachePath();
+
+        $cache = $this->getCache();
+        $basenames = $this->getAllBasenames();
+        $modules = collect();
+
+        $basenames->each(function ($module, $key) use ($modules, $cache) {
+            $basename = collect(['basename' => $module]);
+            $temp = $basename->merge(collect($cache->get($module)));
+            $manifest = $temp->merge(collect($this->getManifest($module)));
+            $modules->put($module, $manifest);
+        });
+
+        $modules->each(function ($module) {
+            $module->put('id', crc32($module->get('slug')));
+
+            if (!$module->has('enabled')) {
+                $module->put('enabled', config("kathus.locations.$this->location.enabled", true));
+            }
+
+            if (!$module->has('order')) {
+                $module->put('order', 9001);
+            }
+
+            return $module;
+        });
+
+        $content = json_encode($modules->all(), JSON_PRETTY_PRINT);
+
+        return $this->files->put($cachePath, $content);
+    }
+
+    /**
+     * Get the contents of the cache file.
+     *
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    private function getCache()
+    {
+        $cachePath = $this->getCachePath();
+
+        if (!$this->files->exists($cachePath)) {
+            $this->createCache();
+
+            $this->optimize();
+        }
+
+        return collect(json_decode($this->files->get($cachePath), true));
+    }
+
+    /**
+     * Create an empty instance of the cache file.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function createCache()
+    {
+        $cachePath = $this->getCachePath();
+        $content = json_encode([], JSON_PRETTY_PRINT);
+
+        $this->files->put($cachePath, $content);
+
+        return collect(json_decode($content, true));
+    }
+
+    /**
+     * Get the path to the cache file.
+     *
+     * @return string
+     */
+    private function getCachePath()
+    {
+        if (!$this->files->isDirectory(storage_path("app/kathus"))) {
+            $this->files->makeDirectory(storage_path("app/kathus"));
+        }
+
+        $location = Str::slug($this->location);
+
+        return storage_path("app/kathus/$location.json");
+    }
+}
--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -1,1 +1,155 @@
 <?php
+
+namespace Kathus\Repositories;
+
+use Exception;
+use Illuminate\Support\Str;
+use Kathus\Contracts\Repository as RepositoryContract;
+use Illuminate\Config\Repository as Config;
+use Illuminate\Filesystem\Filesystem;
+
+abstract class Repository implements RepositoryContract
+{
+    /**
+     * @var string
+     */
+    public $location;
+
+    /**
+     * @var \Illuminate\Config\Repository
+     */
+    protected $config;
+
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * @var string Path to the defined modules directory
+     */
+    protected $path;
+
+    /**
+     * Constructor method.
+     *
+     * @param string $location
+     * @param \Illuminate\Config\Repository $config
+     * @param \Illuminate\Filesystem\Filesystem $files
+     */
+    public function __construct(string $location, Config $config, Filesystem $files)
+    {
+        $this->location = $location;
+        $this->config = $config;
+        $this->files = $files;
+    }
+
+    /**
+     * Get all module Base name.
+     *
+     * @return array
+     */
+    protected function getAllBasenames()
+    {
+        $path = $this->getPath();
+
+        try {
+            $collection = collect($this->files->directories($path));
+
+            $basenames = $collection->map(function ($item, $key) {
+                return basename($item);
+            });
+
+            return $basenames;
+        } catch (\InvalidArgumentException $e) {
+            return collect([]);
+        }
+    }
+
+    /**
+     * Get a module's manifest contents.
+     *
+     * @param string $slug
+     * @return \Illuminate\Support\Collection
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function getManifest($slug)
+    {
+        if (!is_null($slug)) {
+            $path = $this->getManifestPath($slug);
+            $contents = $this->files->get($path);
+            $validate = @json_decode($contents, true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $collection = collect(json_decode($contents, true));
+
+                return $collection;
+            }
+
+            throw new Exception('[' . $slug . '] Your JSON manifest file was not properly formatted. Check for formatting issues and try again.');
+        }
+    }
+
+    /**
+     * Get modules path.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path ?: $this->config->get("kathus.locations.$this->location.path");
+    }
+
+    /**
+     * Set modules path in "RunTime" mode.
+     *
+     * @param string $path
+     * @return object $this
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path for the specified module.
+     *
+     * @param string $slug
+     * @return string
+     */
+    public function getModulePath($slug)
+    {
+        $module = Str::studly($slug);
+
+        if (\File::exists($this->getPath() . "/{$module}/")) {
+            return $this->getPath() . "/{$module}/";
+        }
+
+        return $this->getPath() . "/{$slug}/";
+    }
+
+    /**
+     * Get path of module manifest file.
+     *
+     * @param $slug
+     * @return string
+     */
+    protected function getManifestPath($slug)
+    {
+        $filename = config("kathus.locations.$this->location.manifest") ?: 'module.json';
+
+        return $this->getModulePath($slug) . $filename;
+    }
+
+    /**
+     * Get modules namespace.
+     *
+     * @return string
+     */
+    public function getNamespace()
+    {
+        return rtrim($this->config->get("kathus.locations.$this->location.namespace"), '/\\');
+    }
+}
